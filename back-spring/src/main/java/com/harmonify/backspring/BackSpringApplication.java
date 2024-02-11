127b339d10af9ab4c6f43e9b5793ca5b3d0195e5
Thiago Barros <bunbismuth@proton.me>
2024-02-10 22:26:57 -0300

    feat: criação do projeto Spring

---
+++ back-spring/src/main/java/com/harmonify/backspring/BackSpringApplication.java
@@ -0,0 +1,13 @@
+package com.harmonify.backspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackSpringApplication.class, args);
+	}
+
+}
---
+++ back-spring/src/test/java/com/harmonify/backspring/BackSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harmonify.backspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
