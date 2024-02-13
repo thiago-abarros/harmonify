ce659f4e405500dc46e03555c84e88a7a935771c
Thiago Barros <bunbismuth@proton.me>
2024-02-13 14:14:11 -0300

    refactor: componente de música atualizado
    Descrição:
    Alterações para construção de um service que consome uma API para recuperar as músicas.
    Também foi adicionado um spinner para mostrar quando os dados estão sendo carregados.

--- front-angular/src/app/musicas/musicas-routing.module.ts
+++ front-angular/src/app/musicas/musicas-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MusicasComponent } from './musicas/musicas.component';
 
-const routes: Routes = [{ path: '', component: MusicasComponent }];
+const routes: Routes = [
+  {path: '', component: MusicasComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class MusicasRoutingModule {}
+export class MusicasRoutingModule { }
