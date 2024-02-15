cdbd41c92d5087c1f8d01a14c42c6e75f40cbd0b
Thiago Barros <bunbismuth@proton.me>
2024-02-15 11:14:34 -0300

    feat: adicionado método emErro para lidar com erros

--- front-angular/src/app/musicas/musicas/musicas.component.ts
+++ front-angular/src/app/musicas/musicas/musicas.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { Musica } from '../modelos/musica';
 import { MusicasService } from '../servicos/musicas.servico';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogoErroComponent } from '../../compartilhado/componentes/dialogo-erro/dialogo-erro.component';
 
 @Component({
   selector: 'app-musicas',
@@ -13,8 +15,21 @@ export class MusicasComponent {
   colunasMostradas: string[];
   fonteDados$: Observable<Musica[]>;
 
-  constructor(private musicasServico: MusicasService) {
-    this.fonteDados$ = this.musicasServico.listarMusicas();
+  constructor(
+    private musicasServico: MusicasService,
+    public dialogo: MatDialog
+    ) {
+    this.fonteDados$ = this.musicasServico.listarMusicas().pipe(
+      catchError( error => {
+        this.emErro('Erro ao carregar músicas.');
+        return of([])
+      }));
     this.colunasMostradas = ['nome', 'artista', 'generoMusical'];
-  }
+    }
+
+    emErro(mensagemErro: string) {
+      this.dialogo.open(DialogoErroComponent, {
+        data: mensagemErro
+      });
+    }
 }
