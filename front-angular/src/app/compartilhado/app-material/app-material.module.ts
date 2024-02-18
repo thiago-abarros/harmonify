8ce06d83d3ce687c1558f06786c52f5b7072383c
Thiago Barros <bunbismuth@proton.me>
2024-02-18 20:24:49 -0300

    chore: MatIconModule incluído nas dependências do módulo

--- front-angular/src/app/compartilhado/app-material/app-material.module.ts
+++ front-angular/src/app/compartilhado/app-material/app-material.module.ts
@@ -5,6 +5,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   exports: [
@@ -13,7 +14,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatToolbarModule,
     MatProgressSpinnerModule,
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
 })
 export class AppMaterialModule { }
