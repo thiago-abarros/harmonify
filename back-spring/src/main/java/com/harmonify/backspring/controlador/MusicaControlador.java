5828c24c3a93a9960b0c976bd3893c45666b32fb
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-02-21 18:13:10 -0300

    feat: criação da classe MusicaControlador

---
+++ back-spring/src/main/java/com/harmonify/backspring/controlador/MusicaControlador.java
@@ -0,0 +1,30 @@
+package com.harmonify.backspring.controlador;
+
+import com.harmonify.backspring.dominio.Musica;
+import com.harmonify.backspring.dominio.dto.MusicaDTO;
+import com.harmonify.backspring.servico.MusicaServico;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/musicas")
+public class MusicaControlador {
+
+  @Autowired
+  MusicaServico musicaServico;
+
+  @GetMapping
+  public List<Musica> listarMusicas() {
+    return null;
+  }
+
+  @PostMapping("/salvar")
+  public void salvarMusica(@RequestBody MusicaDTO musicaDTO) {
+    musicaServico.salvar(musicaDTO);
+  }
+}
