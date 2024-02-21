cad6784bcc19ad1731f54626f6a27ac6f7577ddc
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-02-21 18:10:55 -0300

    feat: criação da classe MusicaDTO

---
+++ back-spring/src/main/java/com/harmonify/backspring/dominio/dto/MusicaDTO.java
@@ -0,0 +1,19 @@
+package com.harmonify.backspring.dominio.dto;
+
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class MusicaDTO {
+
+  private String nome;
+  private String artista;
+  private String generoMusical;
+  private String duracao;
+  private Date lancamento;
+  private byte[] arquivo;
+}
