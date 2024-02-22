fffccfd381db6a70f748470c1c4087293df8158c
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-02-22 00:28:24 -0300

    feat: redirecionamento do endpoint get para o backend

--- front-angular/src/app/musicas/servicos/musicas.servico.ts
+++ front-angular/src/app/musicas/servicos/musicas.servico.ts
@@ -14,7 +14,7 @@ export class MusicasService {
   constructor(private httpClient: HttpClient) { }
 
   listarMusicas() : Observable<Musica[]> {
-    return this.httpClient.get<Musica[]>(this.API)
+    return this.httpClient.get<Musica[]>(this.API_SPRING + '/listar')
       .pipe(
         first(),
         delay(3000),
