0c5bc96c7191d13554313c006cc6db5c08794cb5
Thiago Barros <bunbismuth@proton.me>
2024-02-24 13:26:31 -0300

    refactor: reformatado para os padrões da Google

--- back-spring/src/main/java/com/harmonify/backspring/BackSpringApplication.java
+++ back-spring/src/main/java/com/harmonify/backspring/BackSpringApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackSpringApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BackSpringApplication.class, args);
+  }
 
 }
--- back-spring/src/main/java/com/harmonify/backspring/repositorio/MusicaRepositorio.java
+++ back-spring/src/main/java/com/harmonify/backspring/repositorio/MusicaRepositorio.java
@@ -3,6 +3,6 @@ package com.harmonify.backspring.repositorio;
 import com.harmonify.backspring.dominio.Musica;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MusicaRepositorio extends JpaRepository<Musica,Long> {
+public interface MusicaRepositorio extends JpaRepository<Musica, Long> {
 
 }
